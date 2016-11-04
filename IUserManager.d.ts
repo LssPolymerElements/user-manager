816d8df367de7c5b94cb7a75de4248cea81b3c62
Aaron Drabeck <aaron-drabeck@leavitt.com>
2016-11-03 21:57:21 -0600

    removes promise reference

--- IUserManager.d.ts
+++ IUserManager.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="node_modules/@types/es6-promise/index.d.ts" />
+/// <reference path="user.d.ts" />
 /// <reference types="es6-promise" />
 declare var fetch: any;
 interface IUserManager {
--- IUserManager.js
+++ IUserManager.js
@@ -1,1 +1,1 @@
-/// <reference path="./node_modules/@types/es6-promise/index.d.ts" />
+/// <reference path="./user.ts" />
