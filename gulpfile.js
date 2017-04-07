ff43005083a263c8eeb316ec5606e008ddbf59b3
Aaron Drabeck <aaron-drabeck@leavitt.com>
2017-04-07 10:09:05 -0600

    better gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ var exec = require('child_process').exec;
 var browserSync = require('browser-sync').create();
 
 gulp.task('compile', function (done) {
-  exec('tsc', function (err, stdOut, stdErr) {
+  exec('tsc -w', function (err, stdOut, stdErr) {
     console.log(stdOut);
-    if (err){
+    if (err) {
       done(err);
     } else {
       done();
@@ -14,9 +14,9 @@ gulp.task('compile', function (done) {
 });
 
 gulp.task('polymerServe', function (done) {
-  exec('polymer serve', function (err, stdOut, stdErr) {
+  exec('polymer serve -p 8000 -v', function (err, stdOut, stdErr) {
     console.log(stdOut);
-    if (err){
+    if (err) {
       done(err);
     } else {
       done();
@@ -24,15 +24,35 @@ gulp.task('polymerServe', function (done) {
   });
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-         proxy: "localhost:8080",
-         files: '*.html, *.js, images/*, demo/*.html'
+// gulp.task('browser-sync', function () {
+//   browserSync.init({
+//     proxy: "localhost:8080",
+//     files: '**/*.html,**/*.js'
+//   });
+// });
+
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    proxy: "localhost:8000"
+  });
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+
+  var directoriesToWatch = ["src/**/*.js", "src/**/*.html", "demo/**/*.html", "demo/**/*.js", "*.js", "*.html", "images/*.*"]
+
+  directoriesToWatch.forEach(function (directory) {
+    console.log('Listening for changes at: ' + directory);
+    var jsWatcher = gulp.watch(directory, {
+      interval: 1000
+    }).on('change', browserSync.reload);
+
+    jsWatcher.on('change', function (event) {
+      console.log('File ' + event.path + ' was ' + event.type);
     });
+  });
+
 });
 
-gulp.task('ts-watch', ['compile'], function() {
-    gulp.watch('./*.ts', ['compile']);
-});
-
-gulp.task('default', ['ts-watch','polymerServe','browser-sync']);
+gulp.task('default', ['compile', 'polymerServe', 'watch']);
